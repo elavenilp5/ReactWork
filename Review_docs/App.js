87110611900e41d693870e163a5666d726b7d50c
elavenilp5 <73893971+elavenilp5@users.noreply.github.com>
2022-05-30 10:41:52 +0530

    Add files via upload

---
+++ Review_docs/App.js
@@ -0,0 +1,17 @@
+import "./App.css"
+import Header from "./Components/Header"
+import DraftTable from "./Components/DraftTable"
+import TableBaseline from "./Components/TableBaseline"
+import Tab from "./Components/Tab"
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* <DraftTable /> */}
+      {/* <TableBaseline /> */}
+      <Tab />
+    </div>
+  )
+}
+
+export default App
